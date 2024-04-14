1b97246cca051ec1cedefb93867b9890ccb3caa1
emanuel <emanuelrodriguezroman@gmail.com>
2024-04-14 16:23:27 -0600

    mask improvement

--- src/api/v1/models/dogs.py
+++ src/api/v1/models/dogs.py
@@ -1,5 +1,8 @@
 import numpy as np
 import io
+import PIL
+from tensorflow import keras
+from tensorflow.keras.utils import load_img
 from PIL import Image
 import tensorflow as tf
 from fastapi.responses import StreamingResponse
@@ -15,24 +18,44 @@ def preprocess_image(image_file):
     image_array = np.expand_dims(image_array, axis=0) / 255.0  # Normalize if your model expects this
     return image_array
 
+def autocontrast_func(img, cutoff=0):
+    '''
+        same output as PIL.ImageOps.autocontrast
+    '''
+    n_bins = 256
+    def tune_channel(ch):
+        n = ch.size
+        cut = cutoff * n // 100
+        if cut == 0:
+            high, low = ch.max(), ch.min()
+        else:
+            hist = cv2.calcHist([ch], [0], None, [n_bins], [0, n_bins])
+            low = np.argwhere(np.cumsum(hist) > cut)
+            low = 0 if low.shape[0] == 0 else low[0]
+            high = np.argwhere(np.cumsum(hist[::-1]) > cut)
+            high = n_bins - 1 if high.shape[0] == 0 else n_bins - 1 - high[0]
+        if high <= low:
+            table = np.arange(n_bins)
+        else:
+            scale = (n_bins - 1) / (high - low)
+            offset = -low * scale
+            table = np.arange(n_bins) * scale + offset
+            table[table < 0] = 0
+            table[table > n_bins - 1] = n_bins - 1
+        table = table.clip(0, 255).astype(np.uint8)
+        return table[ch]
+    channels = [tune_channel(ch) for ch in cv2.split(img)]
+    out = cv2.merge(channels)
+    return out
+    
 def create_image_from_mask(prediction):
     try:
-        # Convert RGB numpy array to a PIL image
-        image = Image.fromarray(prediction.astype('uint8'), 'RGB')
+        """Quick utility to display a model's prediction."""
+        mask = np.argmax(prediction, axis=-1)
+        mask = np.expand_dims(mask, axis=-1)
+        img = PIL.ImageOps.autocontrast(keras.preprocessing.image.array_to_img(mask))
+        return img
 
-        # Convert RGB image to Grayscale
-        gray_image = image.convert('L')
-
-        # Convert grayscale image to numpy array
-        gray_array = np.array(gray_image)
-
-        # Apply threshold to create a binary mask
-        # You might need to adjust the threshold value based on your specific case
-        mask = (gray_array > 128).astype(np.uint8) * 255  # Example threshold of 128
-
-        # Convert the binary mask back to a PIL image
-        mask_image = Image.fromarray(mask, mode='L')
-        return mask_image
     except Exception as e:
         print('create_image_from_mask :: error: {}'.format(e))
 
